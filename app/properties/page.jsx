015ce9438384117bdf693fb52411bc2f5c52b782
Otabekkadirov <ganijanovich94@gmail.com>
2024-06-29 20:38:08 +0500

    add properties and property-cards

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,21 @@
-"use client";
-import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
 
 const Properties = () => {
   return (
-    <div>
-      <h1 className="text-lg">Properties</h1>
-      <Link href="/" className="text-blue-500">
-        Go Home
-      </Link>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length == 0 ? (
+          <p>No Properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
